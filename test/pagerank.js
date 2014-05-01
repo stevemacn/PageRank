0beff76659ebedaa350bef1b9ba72e4c675948e6
Stephen MacNeil <smacneil01@gmail.com>
2014-05-01 19:46:11 -0400

    add tests for pagerank algorithm

--- test/pagerank.js
+++ test/pagerank.js
@@ -1,34 +1,44 @@
-var assert = require('assert')
+var should = require('should')
   , fs = require('fs')
   , path = require('path')
   , Pagerank = require('../lib/pagerank')
 
-describe('kmeans', function () {
+describe('Page rank', function () {
 
-    linkProb = 0.85 //Larger numbers (0.85) provide lower chance of random links 
-    tolerance = 0.0001 //accuracy at which we terminate 
+    //Larger numbers (0.85) provide low chance of random links 
+    linkProb = 0.85 
+    
+    //accuracy at which we terminate 
+    tolerance = 0.0001 
+    
+    var nodeMatrix = [
+        [1,2],[],[0,1,4],[4,5],[3,5],[3]
+    ]
+    
+    var nodeMatrix = [
+        [1],[0,2],[0,3,4],[4,5],[2,6],[0,6],[3]
+    ]
+            
+    expectedResponse = [ 
+        0.1751523680914745,
+        0.17030808430632474,
+        0.1505779562978131,
+        0.1633947196406794,
+        0.13353508156024055,
+        0.09087132727586017,
+        0.11680129518391424
+    ]
 
     it('correctly ranks nodes', function (done) {
 
-
-        var nodeMatrix = [
-            [1,2],[],[0,1,4],[4,5],[3,5],[3]
-        ]
-
-
-        var nodeMatrix = [
-            [1],[0,2],[0,3,4],[4,5],[2,6],[0,6],[3]
-        ]
-
-
-
         Pagerank(nodeMatrix, linkProb, tolerance, function (err, res) {
-            console.log("____RESULTS____")
-            console.log(res)
-            //assert.equals(err, null)
+            
+            should.not.exist(err)
+            should.exist(res)
+            res.should.be.instanceof(Array).and.have.lengthOf(7)
+            res.should.eql(expectedResponse)
         })
     
-        //asert.equal(,)
-    done()
-  })
+        done()
+    })
 })
